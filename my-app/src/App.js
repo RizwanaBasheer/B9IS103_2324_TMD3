c483eed2016915ff4f5110cb12df2e8a8347f9ef
RizwanaBasheer <20031946@mydbs.ie>
2024-06-25 11:44:14 +0100

    js update for the app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {TextField, List, ListItem, ListItemText, Paper, Button} from '@mui/material';
 
-function App() {
+const App = () => {
+  const [messages, setMessages] =React.useState([]);
+  const [input, setInput] =React.useState(''); 
+  
+  const handleSendMessage =() =>{
+    if (input.trim()){
+      setMessages([...messages,input]);
+      setInput('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+     <div className="row justify-content-center">
+      <div className="col-md-6">
+       <h1 className="text-center">Chat Application</h1>
+       <Paper style={{height:'60vh', overflow:'auto', marginBottom: '1rem'}}>
+        <List>
+          {messages.map((message, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={message}/>
+            </ListItem>
+          ))}
+        </List>
+       </Paper>
+       <TextField
+            fullWidth
+            label="Type a message"
+            variant="outlined"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                handleSendMessage();
+              }
+            }}/>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            className="mt-3" 
+            onClick={handleSendMessage}>
+            Send
+          </Button>
+      </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
