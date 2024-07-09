bbf9da6b20b3d16ebf33fe15372d71ca8154305f
RizwanaBasheer <20031946@mydbs.ie>
2024-07-09 15:00:52 +0100

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
@@ -6,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+mongoose.connect("mongodb+srv://rizwanabasheer067:hNzeKAhi1VEyfUoQ@inote.mbucrtj.mongodb.net/chat_app?retryWrites=true&w=majority&appName=INote")
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
