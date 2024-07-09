5bb080f5937626fe5498a756afcb67505c484a55
RizwanaBasheer <20031946@mydbs.ie>
2024-07-09 15:12:20 +0100

    user creation

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  email: String,
+  password: String,
+  
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
