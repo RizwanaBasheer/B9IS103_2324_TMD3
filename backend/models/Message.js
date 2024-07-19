2f3993f84c2f387602f24064f09f1d8e67d63314
RizwanaBasheer <20031946@mydbs.ie>
2024-07-19 08:42:04 +0100

    Message Model

---
+++ backend/models/Message.js
@@ -0,0 +1,13 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  content: String,
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
+
