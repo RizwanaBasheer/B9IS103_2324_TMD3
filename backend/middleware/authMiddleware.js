c9d34b5bb25ae08748463d03ffc19a62bcc62f11
RizwanaBasheer <20031946@mydbs.ie>
2024-07-19 09:06:28 +0100

    authmiddleware js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+// middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+exports.authenticateJWT = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).send('Access Denied');
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    console.log(verified);
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid Token');
+  }
+};
