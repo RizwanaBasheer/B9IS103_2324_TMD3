946d3ab66bc81296baf20c0d1d53814dfd0bbca2
Ranjithkumar-Mathiazhagan <ranjithvoc32@gmail.com>
2024-07-19 20:10:20 +0100

    Update App js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,20 @@
 import React from 'react';
-import {TextField, List, ListItem, ListItemText, Paper, Button} from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from '@mui/material';
+import Home from './components/Home';
+import ChatApp from './components/ChatApp';
 
-const App = () => {
-  const [messages, setMessages] =React.useState([]);
-  const [input, setInput] =React.useState(''); 
-  
-  const handleSendMessage =() =>{
-    if (input.trim()){
-      setMessages([...messages,input]);
-      setInput('');
-    }
-  };
-
+function App() {
   return (
-    <div className="container mt-5">
-     <div className="row justify-content-center">
-      <div className="col-md-6">
-       <h1 className="text-center">Chat Application</h1>
-       <Paper style={{height:'60vh', overflow:'auto', marginBottom: '1rem'}}>
-        <List>
-          {messages.map((message, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={message}/>
-            </ListItem>
-          ))}
-        </List>
-       </Paper>
-       <TextField
-            fullWidth
-            label="Type a message"
-            variant="outlined"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                e.preventDefault();
-                handleSendMessage();
-              }
-            }}/>
-          <Button 
-            variant="contained" 
-            color="primary" 
-            className="mt-3" 
-            onClick={handleSendMessage}>
-            Send
-          </Button>
-      </div>
-      </div>
-    </div>
+    <Router>
+      <Container fluid>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<ChatApp />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-};
+}
 
 export default App;
