5ba4d1e28e0d911df85528506c28998a909e1858
himanshu <hv7460@gmail.com>
2024-07-22 16:04:48 +0100

    for production build

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,20 @@ require('./sockets/chatSocket'); // Initialize socket
 const port = process.env.PORT_SERVER || 5000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 const MONGOURI = "mongodb+srv://rizwanabasheer067:hNzeKAhi1VEyfUoQ@inote.mbucrtj.mongodb.net/chat_app?retryWrites=true&w=majority&appName=INote"
 mongoose.connect(MONGOURI)
