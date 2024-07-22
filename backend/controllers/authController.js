da707325826f1e73257e4ffe7bfb077174e14577
RizwanaBasheer <20031946@mydbs.ie>
2024-07-22 08:43:35 +0100

    updated auth files

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,9 @@
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const PrivateKey = require('../models/PrivateKey');
+const PublicKey = require('../models/PublicKey');
+const { encrypt, decrypt } = require('../utils/encryption'); 
 
 exports.googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
 
@@ -17,5 +20,45 @@ exports.googleAuthCallback = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.getUserKeys = async (req, res) => {
+  try {
+
+    console.log(req.user);
+    const encryptedUserId = encrypt(req.user.id.toString());
+    console.log(encryptedUserId);
+
+    const publicKeyDoc = await PublicKey.findOne({ userId: encryptedUserId });
+    const privateKeyDoc = await PrivateKey.findOne({ userId: encryptedUserId });
+
+    console.log(publicKeyDoc);
+    console.log(privateKeyDoc);
+
+    if (!publicKeyDoc || !privateKeyDoc) {
+      return res.status(404).json({ error: 'Keys not found' });
+    }
+
+    // Decrypt keys
+    const publicKeyPEM = decrypt(publicKeyDoc.publicKey);
+    const privateKeyPEM = decrypt(privateKeyDoc.privateKey);
+
+    res.status(200).json({ publicKey: publicKeyPEM, privateKey: privateKeyPEM });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).send('Access Denied');
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid Token');
   }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,14 @@ const express = require('express');
 const passport = require('passport');
 const authController = require('../controllers/authController');
 const router = express.Router();
+const { authenticateJWT } = require('../middleware/authMiddleware');
 
 router.get('/google', authController.googleAuth);
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), authController.googleAuthCallback);
+
+router.get('/user/keys', authenticateJWT, authController.getUserKeys);
+
+router.get('/verify', authController.verifyToken);
 
 module.exports = router;
